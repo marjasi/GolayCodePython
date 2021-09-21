a54271515cfd065ce4651904e9077877b50c284a
MellowJay <marijus.jasinskas@mif.stud.vu.lt>
2021-09-21 20:19:00 +0300

    Updated .gitignore, added setup.py to generate an exe file of the program.

--- vector.py
+++ vector.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class Vector:
     """Vektoriaus klase.
 
@@ -12,8 +13,13 @@ class Vector:
     def __init__(self, vectorElements: List[int]):
         """Konstruktoriaus metodas.
 
-        Sukuriamas vektorius su nurodytais elementais vectorElements.
-        Elementai turi buti int tipo sveikieji skaiciai.
+        Sukuriamas vektorius su nurodytais masyvo vectorElements elementais.
+        Masyvo vectorElements elementai yra nukopijuojami i nauja masyva sukurta vektoriaus objektui.
+        Elementai turi buti integer tipo sveikieji skaiciai.
         """
 
-        self.elements = vectorElements
+        self.elements = vectorElements.copy()
+
+
+print("It works, my dude.")
+input("Press Enter to continue...")
