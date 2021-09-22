d9a45ed34b58febd7c81e8305899b37c7045ff17
MellowJay <marijus.jasinskas@mif.stud.vu.lt>
2021-09-22 16:03:31 +0300

    Implemented communication channel with error probability.

---
+++ comm_channel.py
@@ -0,0 +1,45 @@
+from typing import List
+import random
+
+
+def distort_binary_info(binaryNumber: int) -> int:
+    """Metodas skirtas binariniu duomenu iskraipymui.
+
+    Metodas 0 pavercia 1, o 1 pavercia 0.
+    """
+
+    return 0 if binaryNumber == 1 else 1
+
+
+class CommChannel:
+    """Kanalo klase.
+
+    Kiekvienas siunciamas simbolis kanalu yra iskraipomas su nurodyta tikimybe.
+    """
+
+    # Kanalo iskraipymo tikimybe.
+    probOfError = None
+
+    def __init__(self, probOfError: float):
+        """Konstruktoriaus metodas.
+
+        Sukuriamas komunikacijos kanalas su iskraipymo tikimybe probOfError.
+        Viena karta inicializuojamas atsitiktiniu skaiciu generatorius.
+        """
+
+        self.probOfError = probOfError
+        random.seed()
+
+    def send_binary_info(self, binaryInfo: List[int]) -> List[int]:
+        """Metodas skirtas binariniu duomenu persiuntimui kanalu.
+
+        Sugeneruojamas realus skaicius intervale [0, 1] ir pagal iskraipymo tikimybe
+         nusprendziama ar informacija bus iskraipyta.
+        Po siuntimo metodas grazina atsiusta binarines informacijos sarasa.
+        """
+
+        for index, element in enumerate(binaryInfo):
+            if random.random() <= self.probOfError:
+                binaryInfo[index] = distort_binary_info(element)
+
+        return binaryInfo
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from golay_code import generate_matrix_b
+from vector import Vector
+from comm_channel import *
 
 
 def main():
     generate_matrix_b()
-    print("It works, my dude.")
+    vector = Vector([0, 1, 1, 1, 0])
+    channel = CommChannel(0.1)
+    print("Vector before being sent")
+    print(vector)
+    vector.elements = channel.send_binary_info(vector.elements)
+    print("Vector after being sent")
+    print(vector)
 
 
 if __name__ == "__main__":
