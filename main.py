727329ee8b2455083a9e7d5a987010937356b1dc
MellowJay <marijus.jasinskas@mif.stud.vu.lt>
2021-09-22 19:10:08 +0300

    Implemented matrice B, G and H generation.

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from golay_code import generate_matrix_b
+from golay_code import *
 from vector import Vector
 from comm_channel import *
 
 
 def main():
-    matrixB = generate_matrix_b()
+    matrixB = generate_matrix_h()
     print(matrixB)
-    vector = Vector([0, 1, 1, 1, 0])
-    channel = CommChannel(0.1)
+    vector = Vector([0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0])
+    channel = CommChannel(0.2)
     print("Vector before being sent")
     print(vector)
     vector.elements = channel.send_binary_info(vector.elements)
